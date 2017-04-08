27f408e618ac5365a42409c221423bbb7ab4cb88
sfraguas <stella.fraguas@gmail.com>
2017-04-08 14:56:20 -0300

    Update test.php

--- test.php
+++ test.php
@@ -1,4 +1,10 @@
 <?php
   echo "HELLO WORLD!";
+
   include "connect_sql.php";
+
+  echo "host: " . $server;
+  echo "\n user: " . $username;
+  echo "\n password: " . $password;
+  echo "\n schema: " . $db;
 ?>
